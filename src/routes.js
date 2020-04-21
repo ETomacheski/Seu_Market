4a4138f49993edee3a7820cbf1aa1bd64a5a0401
Eduardo Dias <eduardodiasprog@gmail.com>
2020-04-21 18:38:16 -0300

    removendo identação desnecessaria

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,8 @@
 const express = require('express');
-
 const routes = express.Router();
 
-
-routes.get('/',(req,res)=>{
-    res.end("Hello");
+routes.get('/', (req,res) =>{
+    res.end('Hello');
 });
-
-
 
 module.exports = routes;
