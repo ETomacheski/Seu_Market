ef983daa9b0e6c83304a32ebbb695e1e6b3ee249
ETomacheski <tomacheski2001@gmail.com>
2020-04-21 19:43:57 -0300

    config body Parser

--- src/app.js
+++ src/app.js
@@ -1,12 +1,16 @@
 const path = require('path');
 const express = require('express');
 const app = express();
+const bodyParser = require("body-parser");
+
 const routes = require('./routes');
 
 app.use(express.json());
 app.use(express.static('./src/public'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
 app.use(routes);
 
 module.exports = app;
