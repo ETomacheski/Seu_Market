836654f8da682cddafa6ea4623aec8c48702fed9
Eduardo Dias <eduardodiasprog@gmail.com>
2020-04-21 18:37:11 -0300

    configurando o banco

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,9 @@
 module.exports = {
-    dialect: 'mysql',
-    host:'localhost',
-    username: 'root',
-    password: 'portoalegre',
-    database:'sqlnode',
+    dialect: 'postgres',
+    host: 'localhost',
+    username: 'postgres',
+    password: '123456',
+    database: 'izi_compras',
     define:{
         timestamps:true,
         underscored:true,
--- src/database/index.js
+++ src/database/index.js
@@ -1,9 +1,5 @@
 const Sequelize = require('sequelize');
 const dbconfig = require('../config/database');
-
-
 const connection = new Sequelize(dbconfig);
-
-
 
 module.exports = connection;
