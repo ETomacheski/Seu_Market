b2665c965452d3add89e88bb712ca671406d3fbe
Eduardo Dias <eduardodiasprog@gmail.com>
2020-04-21 19:46:50 -0300

    url extended no bodyparser

--- src/app.js
+++ src/app.js
@@ -5,11 +5,10 @@ const bodyParser = require("body-parser");
 
 const routes = require('./routes');
 
-app.use(express.json());
 app.use(express.static('./src/public'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(routes);
 
