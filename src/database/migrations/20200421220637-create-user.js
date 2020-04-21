91b75c8b20d790e2a62d287d3d4762154df86b56
Eduardo Dias <eduardodiasprog@gmail.com>
2020-04-21 20:00:18 -0300

    ajustando a timestamp

--- src/database/migrations/20200421220637-create-user.js
+++ src/database/migrations/20200421220637-create-user.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const sequelize = require('sequelize');
+
 module.exports = {
   up: (queryInterface, Sequelize) => {
     
@@ -40,10 +42,12 @@ module.exports = {
           },
           created_at:{
             type: Sequelize.DATE,
+            defaultValue: sequelize.literal('NOW()'),
             allowNull:false,
           },
           updated_at:{
             type: Sequelize.DATE,
+            defaultValue: sequelize.literal('NOW()'),
             allowNull:false,
           },
        });
