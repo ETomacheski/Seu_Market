042a9ed455937ea7f2c13275859e425e9b4a16ae
Eduardo Dias <eduardodiasprog@gmail.com>
2020-04-22 17:26:40 -0300

    configurando o cadastro

--- src/config/controllers/UserController.js
+++ src/config/controllers/UserController.js
@@ -2,26 +2,25 @@ const User = require('../../models/user');
 
 module.exports = {
 
-    async index(req,res){
+    async index(req,res) {
         const users = await User.findAll();
         console.log(users)
         return res.json(users);
     },
 
-    async create(req,res){
+    async create(req,res) {
 
         const { name, email,password, delivery_time, city, phone } = req.body;
-        var image;
+        var image = null;
         
         if (req.file != null) {
-            image = req.file.path;
-           
+            image = req.file.path;          
         }
-        console.log({name,email, password,cnpj:"1234",image,delivery_time,phone})
-        const user = await User.create({name,email, password,cnpj:"1234",image:"algo",delivery_time,phone});
-    
+
+        const user = await User.create({name, email, password, image: image, delivery_time, phone});
         console.log(user)
-        return res.end('usuario cadastrado com sucesso')
-        
+
+        return res.redirect('/');
+    
     }
 }
