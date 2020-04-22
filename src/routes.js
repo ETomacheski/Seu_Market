fc977474ddd25e3fd29f25036b6ee46d72df475e
Eduardo Dias <eduardodiasprog@gmail.com>
2020-04-21 23:33:22 -0300

    configurando o multer

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,26 @@
+const path = require('path')
 const express = require('express');
 const routes = express.Router();
+
+const multer  = require('multer');
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/')
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + path.extname(file.originalname)) //Appending extension
+    }
+});
+
+const upload = multer({ storage: storage });
 
 routes.get('/cadastro', (req,res) => {
     res.render('register');
 });
 
-routes.post('/cadastro', (req, res) => {
+routes.post('/cadastro', upload.single('image'), (req, res) => {
     console.log(req.body);
+    console.log(req.file.path);
 });
 
 module.exports = routes;
