f38fb522f0c8b7ed07bcef9af727eb8de73c94ba
ETomacheski <tomacheski2001@gmail.com>
2020-04-22 19:02:55 -0300

    Product list and create

---
+++ src/controllers/ProductController.js
@@ -0,0 +1,41 @@
+const User = require('../models/user');
+const Product = require('../models/Product');
+
+
+module.exports = {
+    async index(req,res){
+        const {id} = req.params;
+
+        const user = await User.findByPk(id, {
+            include: { association : 'products' }
+        });
+
+        return res.json(user);
+    },
+    async create(req,res){
+        const { id } = req.params;
+        const {name,description,price,image} = req.body;
+        console.log(id);
+        const user = await User.findByPk(id);
+
+        if(!user){
+            return res.status(400).json({error: 'User not found'});
+        }
+        const user_id = id;
+        const product = await Product.create({
+            name,
+            description,
+            price,
+            image,
+            user_id,
+        })
+
+        return res.json(product);
+    }
+
+
+
+}
+
+
+
--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ const express = require('express');
 const routes = express.Router();
 const multer = require('./multer');
 const UserController = require('./controllers/UserController');
+const ProductController = require('./controllers/ProductController');
 
 routes.get('/cadastro', (req,res) => {
     res.render('register');
@@ -11,5 +12,10 @@ routes.get('/cadastro', (req,res) => {
 routes.post('/cadastro', multer.single('image'), UserController.create);
 routes.get('/usuarios', UserController.index);
 routes.delete('/usuarios/:id', UserController.delete);
+
+
+routes.post('/produtos/:id', multer.single('image'), ProductController.create);
+routes.get('/produtos/:id', multer.single('image'), ProductController.index);
+
 
 module.exports = routes;
