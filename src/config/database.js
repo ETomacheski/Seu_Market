d5f254fd30914ea75efded8fc69924addb3535d6
Eduardo Dias <eduardodiasprog@gmail.com>
2020-04-23 15:57:04 -0300

    consertando bug no charset da validação

--- src/config/database.js
+++ src/config/database.js
@@ -5,8 +5,6 @@ module.exports = {
     password: '123456',
     database: 'izi_compras',
     define:{
-        charset: 'utf8',
-        collate: 'utf8_general_ci',
         timestamps:true,
         underscored:true,
     }
