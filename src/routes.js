ca27b41ac50e63a70bc5097d463ae41ebc88d747
Eduardo Dias <eduardodiasprog@gmail.com>
2020-04-25 17:11:23 -0300

    criando rota de contato e carrinho

--- src/routes.js
+++ src/routes.js
@@ -9,35 +9,54 @@ const ProductController = require('./controllers/ProductsController');
 const FilterController = require('./controllers/FiltersController');
 const SessionController = require('./controllers/SessionController');
 
-routes.get('/usuarios', UserController.index);
+// ROUTES 
+
 routes.get('/', (req, res) => {
     res.render('index');
 });
+
+routes.get('/carrinho', (req, res) => {
+    res.render('cart');
+});
+
+routes.get('/contato', (req, res) => {
+    res.render('contact');
+});
+
+
+// Listagem de usuários.
+routes.get('/usuarios', UserController.index);
+
 
 // Cadastro de Usuarios
 routes.get('/cadastro', (req,res) => {
     res.render('register');
 });
-
 routes.post('/cadastro', multer.single('image'), UserController.create);
+
 
 // Get de Usuario unico
 routes.get('/usuario/:id', UserController.uniqueUser);
 
+
 // Update e delete.
 routes.delete('/usuario/:id', UserController.delete);
 routes.put('/usuario/:id', UserController.update);
+
 
 // Produtos
 routes.get('/produtos/:id', ProductController.index);
 routes.post('/produtos/:id', multer.single('image'), ProductController.create);
 routes.delete('/produtos/:id', ProductController.delete);
 
+
 // Pesquisas
 routes.get('/produtos/:id/:name', FilterController.index);
 
+
 // Session
 routes.post('/login', SessionController.store);
+
 
 // Apartir daqui todas as rotas so poderam ser acessadas se estiverem autenticadas
 routes.use(Auth);
