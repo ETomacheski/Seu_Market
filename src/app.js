d73f47df926a55a2af74a76a9a054b31d18ad3e7
Eduardo Dias <eduardodiasprog@gmail.com>
2020-04-25 16:58:48 -0300

    criando rota index e upando o bootstrap css

--- src/app.js
+++ src/app.js
@@ -2,23 +2,28 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const session = require('express-session');
-const bodyParser = require("body-parser");
+const bodyParser = require('body-parser');
 
 const routes = require('./routes');
 
 require('./database/index')
-require("dotenv").config();
+require('dotenv').config();
 app.use(session({
 	secret: 'secret',
 	resave: true,
 	saveUninitialized: true
 }))
+
+// Frontend configs.
 app.use(express.static('./src/public'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+// Parser configs.
 app.use(bodyParser.text({defaultCharset: 'utf-8'}));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
 app.use(routes);
 
 
--- src/routes.js
+++ src/routes.js
@@ -1,19 +1,24 @@
 const express = require('express');
 const routes = express.Router();
 const multer = require('./multer');
+const Auth = require('./config/auth');
+
+// Controllers 
 const UserController = require('./controllers/UsersController');
 const ProductController = require('./controllers/ProductsController');
 const FilterController = require('./controllers/FiltersController');
 const SessionController = require('./controllers/SessionController');
-const Auth = require('./config/auth');
-
 
 routes.get('/usuarios', UserController.index);
+routes.get('/', (req, res) => {
+    res.render('index');
+});
 
 // Cadastro de Usuarios
 routes.get('/cadastro', (req,res) => {
     res.render('register');
 });
+
 routes.post('/cadastro', multer.single('image'), UserController.create);
 
 // Get de Usuario unico
@@ -31,16 +36,10 @@ routes.delete('/produtos/:id', ProductController.delete);
 // Pesquisas
 routes.get('/produtos/:id/:name', FilterController.index);
 
-//session
+// Session
 routes.post('/login', SessionController.store);
 
-//Apartir daqui todas as rotas so poderam ser acessadas se estiverem autenticadas
+// Apartir daqui todas as rotas so poderam ser acessadas se estiverem autenticadas
 routes.use(Auth);
-//Rotas que precisam de autorização
-
-routes.get('/teste',(req,res)=>{
-    res.end("FUnciona");
-})
-
 
 module.exports = routes;
