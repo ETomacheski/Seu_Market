cb563f06d2c10318afcc37c7576f432c7b3367aa
ETomacheski <tomacheski2001@gmail.com>
2020-04-24 22:12:22 -0300

    banco na nuvem

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,19 @@
 module.exports = {
     dialect: 'postgres',
-    host: 'localhost',
-    username: 'postgres',
-    password: '123456',
-    database: 'izi_compras',
+    dialectOptions: {
+        ssl: {
+          require: true,
+          // Ref.: https://github.com/brianc/node-postgres/issues/2009
+          rejectUnauthorized: false,
+        },
+        keepAlive: true,        
+      },      
+      ssl: true,
+    host: 'ec2-54-159-112-44.compute-1.amazonaws.com',
+    port:5432,
+    username: 'burhcephzmljnu',
+    password: 'e3649adf22ce9c15f8185b432af25729abac4f1bcd0e7997f60ff2151db9a195',
+    database: 'd8rgq0i1a8rord',
     define:{
         timestamps:true,
         underscored:true,
