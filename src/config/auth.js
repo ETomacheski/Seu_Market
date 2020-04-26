fcd269c67dac197b8c1664afd5cb74daf44aed1b
ETomacheski <tomacheski2001@gmail.com>
2020-04-26 20:07:22 -0300

    arrumando redirecionamento

--- src/config/auth.js
+++ src/config/auth.js
@@ -5,7 +5,7 @@ module.exports = async (req,res,next)=>{
     const authHeader = req.session.token;
 
     if(!authHeader){
-        return res.status(401).redirect('/');
+        return res.status(401).redirect('/login');
     }
 
     //const [, token] = authHeader.split(' ');
