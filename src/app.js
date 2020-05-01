c64a4965e5f68296256941c4b934da825049d259
Eduardo Dias <eduardodiasprog@gmail.com>
2020-04-30 22:54:22 -0300

    adicionando o pacote do helmet no app e adicionando limite de requisições por página

--- src/app.js
+++ src/app.js
@@ -3,16 +3,28 @@ const express = require('express');
 const app = express();
 const session = require('express-session');
 const bodyParser = require('body-parser');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const routes = require('./routes');
 
-require('./database/index')
+require('./database/index');
 require('dotenv').config();
+
+// Security configs
 app.use(session({
 	secret: 'secret',
 	resave: true,
 	saveUninitialized: true
-}))
+}));
+app.use(helmet());
+
+const limiter = rateLimit({ 
+	windowMs: 1 * 60 * 1000,
+	max: 10000
+	// Limit of 10000 requests a minute.
+}); 
+app.use(limiter)
 
 // Frontend configs.
 app.use(express.static(path.join(__dirname, 'public')));
@@ -28,6 +40,7 @@ app.use(function(req, res, next) {
 	res.locals.user = req.session.user_id;
 	next();
 });
+
 
 app.use(routes);
 
